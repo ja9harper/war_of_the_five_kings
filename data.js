cb4893662a25ceb97c2340a28c8cce975e39fcd2
h4w5 <philip@h4w5.com>
2014-11-01 13:30:28 -0400

    further refined concept of new data analysis step

--- data.js
+++ data.js
@@ -1,3 +1,8 @@
+// TODO: don't use the term lands -- maybe domains? estates?
+//       having sea lands is too weird
+
+
+
 /* ************************************************************************** *
  *                          DATA SETS OF WESTEROS                             *
  * ************************************************************************** */
@@ -374,7 +379,8 @@ data.kings = [
     //   their influence as power to every fight!
     influence:   4, // the king is cruel and cowardly, but his family is strong
                     // also: sitting the Iron Throne adds + 4 to influence
-    landIndices: [30, 31, 25, 26, 27, 28, 29],
+    seaPower:    0, // a multiplier for fighting at sea (ie, when sea == true)
+    landIndices: [28, 29, 30, 31, 32, 33, 35, 38],
     warCry:      ' demands allegiance, and orders his men to fight!',
     allies:      [],
     enemies:     ['Stannis', 'Renly', 'Robb'],
@@ -384,7 +390,8 @@ data.kings = [
     name:        'Stannis',
     house:       'Baratheon',
     influence:   4, // the red woman inspires terror, but Stannis does not inspire love...
-    landIndices: [32, 33, 34],
+    seaPower:    2,
+    landIndices: [34, 36, 37, 40],
     warCry:      ' calls upon his god to bring fire to his enemies!',
     allies:      [],
     enemies:     ['Joffrey', 'Renly', 'Robb'],
@@ -393,8 +400,10 @@ data.kings = [
   {
     name:        'Renly',
     house:       'Baratheon',
-    influence:   6, // the people love him, and his guard is devoted! but there are rumors...
-    landIndices: [39, 40, 41, 42, 43, 44, 45, 35, 36, 37, 38],
+    influence:   6, // the people love him, and his guard is devoted! But there are rumors...
+    seaPower:    0,
+    landIndices: [39, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                  50, 51, 52, 53],
     warCry:      ' marches gloriously to battle!',
     allies:      ['Robb'],
     enemies:     ['Joffrey'],
@@ -404,8 +413,9 @@ data.kings = [
     name:        'Balon',
     house:       'Greyjoy',
     influence:   8, // the Ironborn are sure to rise again, and have nothing to lose!
-    landIndices: [24],
-    warCry:      ' sails to war with his reavers!',
+    seaPower:    4,
+    landIndices: [27],
+    warCry:      ' sails to war with his Reavers!',
     allies:      [],
     enemies:     ['Robb'],
     other:       ['Joffrey', 'Stannis', 'Renly']
@@ -414,8 +424,9 @@ data.kings = [
     name:        'Robb',
     house:       'Stark',
     influence:   6, // the young wolf is untested, but his virtue is true
-    landIndices: [3, 4, 5, 6, 7, 8, 9, 10, 11,
-                  12, 13, 14, 15, 16, 17, 18, 19],
+    seaPower:    -2,
+    landIndices: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                   15, 16, 17, 18, 19, 20, 21],
     warCry:      ', the Young Wolf, demands vengeance!',
     allies:      ['Renly'],
     enemies:     ['Joffrey'],
@@ -423,7 +434,73 @@ data.kings = [
   }
 ];
 
-data.neutral = [0, 1, 2, 20, 21, 22, 23, 46, 47, 48, 49, 50, 51, 52, 53];
+data.neutral = [0, 1, 2, 22, 23, 24, 25, 26, 46, 47, 48, 49,
+                50, 51, 52, 53, 54, 56, 57, 58, 59, 60, 61, 62];
+
+// welcome, maester... stuff about citadel, etc.
+
+// our maps are old and dusty, and our men unlearned -- it is up to you to make
+// us ready for war.
+
+// we must be able to survey the land! let's work with the above data structures
+// to pull out the necessary information we need to commit to our effort and
+// gain the iron throne!
+
+// ...
+
+// english list
+// printJSONObject(obj, propList)
+
+// printRegion
+// printDomain
+// printKing
+
+// list the domains
+// list the regions (each)
+// find the domains in a region (map)
+// find a region's power (reduce)
+
+// sort the regions by power (sort)
+// sort the regions by number of domains
+// sort the regions by average defense (2 functions -- avg & sortByAvg)
+// sort the regions by median defense...
+
+// list the kings
+// find the allegiance of any given domain
+// list a king's dominion (all their domains)
+// find a king's power
+// list the neutrals
+// list the neutral's power by region
+// sort the neutrals by their average power
+
+// sort the neutral regions by their power-to-defense ratio
+
+// get adjacent domains by any domain's index
+// use select/filter to grab the seas adjacent to any domain
+
+// create a list of sea indices and add it to the data object
+// select/filter out just seas from domain lists
+
+// list the adjacent seas to any domain index
+// list the reachable domains to any domain index:
+  // from any land: adjacent lands and seas, and
+  //                seas adjacent to any adjacent sea
+  // from any sea: adjacent lands and seas, and
+  //               seas adjacent to any adjacent sea, and
+  //               lands adjacent to any adjacent sea
+
+// create a table of reachable lands from any index, and add it to the data object
+  // - a hashmap-style object, where the properties are the domain indices
+  // - each domain-index value is a hashmap-style object where the properties are
+    // - the domain-indices of the reachable lands, and they map to array values
+    // - that include 'direct' (if they are directly adjacent), and
+    // - a list of domain-indices they can be reached thru (seas)
+
+// finally, write a function that maps any domain to a list of reachable domains
+// that include:
+  // an allegiance index
+  // a path index that is the relationship in the reachable table btwn the lands
+
 
 
 var clc = require('cli-color');
@@ -442,32 +519,246 @@ for(var i = 0, len = data.lands.length, land; i < len; i++) {
   }
 }
 
-for(var i = 0, len = data.lands.length, land, message, itother; i < len; i++) {
-  land = data.lands[i];
-  isSea        = land.sea
-  isStronghold = land.type === 'stronghold'
+function allegianceOf(landName) {
+  var foundKing = data.kings.filter(function(king) {
+    return king.landIndices.some(function(landIndex) {
+      return (landName === data.lands[landIndex].name);
+    });
+  });
+  if (foundKing.length === 0) {
+    foundKing = data.neutral.filter(function(land) {
+      return landName === data.lands[land].name;
+    });
+    return foundKing.length === 1 ? {name: 'Neutral'} : null;
+  } else {
+    return foundKing[0];
+  }
+}
 
-  message = '' //land.region + ' ';
+function powerOfKing(kingObj) {
+  // var king = getKing(kingName);
+  // if (king === null) return null;
+
+  return kingObj.landIndices.reduce(function(aggregate, currentLandIndex) {
+    return aggregate += data.lands[currentLandIndex].power;
+  }, 0);
+}
+
+function powerOfRegion(regionName) {
+  // var king = getKing(kingName);
+  // if (king === null) return null;
+
+  return king.landIndices.reduce(function(aggregate, currentLandIndex) {
+    return aggregate += data.lands[currentLandIndex].power;
+  }, 0);
+}
+
+function englishList(list) {
+  var len = list.length;
+
+  if (len == 0) {
+    return '';
+  } else if (len == 1) {
+    return list[0];
+  } else if (len == 2) {
+    return list[0] + ' and ' + list[1];
+  } else {
+    var last_idx = len - 1;
+    var message = list[0];
+    for(var i = 1; i < last_idx; i++) {
+      message += ', ' + list[i];
+    }
+    // oxford comma
+    return message + ', and ' + list[last_idx];
+  }
+}
+
+function adjacentTo(land) {
+  var adjacentLandIndices = land.adjacent;
+
+  return adjacentLandIndices.map(function(idx) {
+    var land = data.lands[idx];
+    land.index = idx;
+    return land;
+  });
+}
+
+function filterSeasFromLands(landList) {
+  return landList.filter(function(adjLand) {
+    return (adjLand.sea == true);
+  });
+}
+
+// function adjacentSeas(land) {
+//   var adjacentLands = adjacentTo(land);
+//
+//
+// }
+
+//DUP ARRAY!
+// function dup(arr) {
+//   return arr.slice(0);
+// }
+//
+// function uniqueArray(arr) {
+//   arr = dup(arr).sort(function(a, b) {return a - b;});
+//   for(var i = 0; i < arr.length; i++) {
+//     if (arr[i] == arr[i+1]) {
+//       arr.splice(i+1,1);
+//     }
+//   }
+//   return arr;
+// }
+//
+// function uniqueMergeArrays() {
+//   var mergedArray = [];
+//   for (var i = 0; i < arguments.length; i++) {
+//     mergedArray = mergedArray.concat(arguments[i]);
+//   }
+//   return uniqueArray(mergedArray);
+// }
+
+function allReachable(land) {
+
+  // get list of adjacenct lands
+  adjacentLands = adjacentTo(land);
+
+  // pick out the adjancenct lands that are seas
+  adjacentSeas =  filterSeasFromLands(adjacentLands);
+
+  // for each sea, get the list of adjacent lands
+  adjacentLandsToSeas = {};
+  adjacentSeas.forEach(function(sea) {
+    adjacentLandsToSeas[sea.index] = adjacentTo(sea).map(function(l) {
+      return l.index;
+    });
+  });
+
+  // create a list of all, unique, adjacent lands -- OR NOT???
+  adjacentIndexList = {};
+
+  land.adjacent.forEach(function(adj) {
+    // console.log(adj)
+    adjacentIndexList[adj] = ['direct'];
+  });
+
+  for(var sea in adjacentLandsToSeas) {
+    // console.log(sea);
+    adjacentIndexList[sea].push('sea');
+    // console.log(adjacentLandsToSeas[sea])
+    adjacentLandsToSeas[sea].forEach(function(adj) {
+      var idx = Object.keys(adjacentIndexList).indexOf(adj.toString());
+
+      if (idx === -1) {
+        // console.log('add with via sea');
+        adjacentIndexList[adj] = [sea];
+      } else {
+        // console.log('push via sea');
+        adjacentIndexList[adj].push(sea);
+      }
+    });
+
+  }
+  return adjacentIndexList;
+
+  // for each adjacent land, add a reference as direct or via (if it can be
+  //   reached in multiple ways)
+
+}
+
+// for(var i = 0, len = data.lands.length, land, message, itother; i < len; i++) {
+//   land = data.lands[i];
+//
+//   message = landToString(land, i, ':', 25);
+//   message += adjacentToString(land);
+//   // message += allegianceToString(land.name);
+//
+//   console.log(message);
+// }
+
+
+
+// for(var i = 0, len = data.kings.length, king, name, influence, power, message; i < len; i++) {
+//   king      = data.kings[i];
+//   name      = king.name;
+//   influence = king.influence;
+//   power     = powerOfKing(king);
+//
+//   message =  pad(name + ':', 8);
+//   message += ' influence ('          + pad(2, influence, '0') + ')';
+//   message += ', power ('             + pad(2, power, '0') + ')';
+//   message += ', total power ('       + pad(2, influence + power, '0') + ')';
+//   message += ', total power at sea(' + pad(2, influence + power + king.seaPower, '0') + ')';
+//
+//   console.log(message);
+// }
+
+function adjacentToString(land) {
+  var adjacentLandIndices = land.adjacent,
+      adjacentLands,
+      adjacentNames,
+      message;
+
+  adjacentLands = adjacentSeas(land);
+
+  adjacentNames = adjacentLands.map(function(adjLand) {
+    var name = adjLand.name.replace('The ',''),
+        adjacentToThisSea;
+    if (adjLand.sea) {
+      message = clc.blue(name) + ': ';
+      // message = clc.underline(' ' + message + ': ');
+
+      adjacentToThisSea = adjacentSeas(adjLand).map(function(l){
+        return l.name;
+      });
+
+      message += '(' + adjacentToThisSea + ')';
+
+    } else {
+      message = ' ' + clc.yellow(name) + ' ';
+      // message = clc.yellow(' ' + message + ' ')
+    }
+
+    return message;
+  });
+
+  return englishList(adjacentNames);
+}
+
+function allegianceToString(land) {
+  var allegiance = allegianceOf(land);
+  return allegiance ?  allegiance.name : clc.red('n/a');
+}
+
+function landToString(land, idx, delimiter, padding) {
+  var isSea        = land.sea,
+      isStronghold = land.type === 'stronghold',
+      isCity       = land.type === 'city',
+      cobbleMessage;
+
+  delimiter = delimiter || '';
+  padding   = padding   || 0;
+
+  if (delimiter) {
+    cobbleMessage = function() {return pad(message + land.name + delimiter, padding);}
+  } else {
+    cobbleMessage = function() {return message + land.name;}
+  }
+
+  message = pad(2, idx.toString(), '0') + ' ';
 
   if (isSea && isStronghold) {
-    message += clc.magenta(pad(land.name + ':', 21));
+    message = clc.magenta(cobbleMessage());
   } else if (isSea) {
-    message += clc.blue(pad(land.name + ':', 21));
+    message = clc.blue(cobbleMessage());
   } else if (isStronghold) {
-    message += clc.yellow(pad(land.name + ':', 21));
-  } else if (land.type === 'city') {
-    message += clc.green(pad(land.name + ':', 21));
+    message = clc.yellow(cobbleMessage());
+  } else if (isCity) {
+    message = clc.green(cobbleMessage());
   } else {
-    message += pad(land.name + ':', 21);
+    message = cobbleMessage();
   }
-  message = message;
-
-  // message += 'P' + pad(2, land.power, '0') + 'D' + pad(2, land.defense, '0');
-  // itother = '    it -->' + land.adjacent.length + ' <> ' + adjacent[i] + ' <-- other';
-  // message += land.adjacent.length == adjacent[i] ? itother : clc.red(itother);
-
-  message += land.adjacent.map(function(adj) {return data.lands[adj].name});
-  console.log(message);
+  return message
 }
 
 // Below you will see this function used... Ignore it or ask us about it! It's
@@ -491,4 +782,4 @@ for(var i = 0, len = data.lands.length, land, message, itother; i < len; i++) {
 // Ignore!
 // exportToModule('data', [lands, kings, neutral, adjacentRegions]);
 
-// module.exports.data = data;
+module.exports.data = data;
